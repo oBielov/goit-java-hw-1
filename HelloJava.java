4c1bdb169cabf1fba4344d1f31d31ee346b2d287
oBielov <45230287+oBielov@users.noreply.github.com>
2021-02-27 22:29:44 +0200

    Add files via upload

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,7 @@
 public class HelloJava {
-  public static void main(String[] args) {
-    System.out.println("Hello Java");
-  }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+
 }
